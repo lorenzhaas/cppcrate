6de18a17d932292f1424f1ee7ad496bc80bf0bf2
Lorenz Haas <lorenz.haas@histomatics.de>
2017-01-14 17:10:29 +0100

    Add gh-pages documentation for v.0.1

---
+++ menudata.js
@@ -0,0 +1,5 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Examples",url:"cppcrateexamples.html"},
+{text:"Classes",url:"annotated.html"},
+{text:"GitHub Repository",url:"https://github.com/lorenzhaas/cppcrate"}]}
